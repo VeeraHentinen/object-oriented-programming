6a7d7bd490e461344de4660b55bfe1e9cd32c99c
veerahentinen@hotmail.com <Styx1369>
2019-01-23 13:43:26 +0200

    Kirjaluokka done

---
+++ Kirjaluokka/Kirjaluokka/Book.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Kirjaluokka
+{
+    class Book
+    {
+        public string title;
+        public string author;
+        public string id;
+        public double price;
+
+        public Book(string title, string author, string id, double price)
+        {
+            this.title = title;
+            this.author = author;
+            this.id = id;
+            this.price = price;
+        }
+
+        public void Print()
+        {
+            Console.WriteLine($"{this.title}");
+            Console.WriteLine($"{this.author}");
+            Console.WriteLine($"{this.id}");
+            Console.WriteLine($"{this.price}");
+        }
+
+        public string ComparePrice(string secondTitle, double secondPrice)
+        {
+            if (this.price > secondPrice)
+                return $"{this.title} on kalliimpi kuin {secondTitle}.";
+            else
+                return $"{secondTitle} on kalliimpi kuin {this.title}.";
+
+        }
+
+    }
+}
---
+++ Kirjaluokka/Kirjaluokka/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Kirjaluokka
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Kirjat:");
+            Console.WriteLine("---------------------");
+
+            Book firstBook = new Book("Kukkuu", "Kekkonen", "50678", 22.50);
+            firstBook.Print();
+
+            Console.WriteLine("---------------------");
+
+            Book secondBook = new Book("Miau", "Kissa", "88905", 16.40);
+            secondBook.Print();
+
+
+             Console.WriteLine("---------------------");
+
+            Console.WriteLine(firstBook.ComparePrice(secondBook.title, secondBook.price));
+        }
+    }
+}
