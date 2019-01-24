52ce49039174e978d175441f53c8ce54d9e41ae9
veerahentinen@hotmail.com <Styx1369>
2019-01-24 09:55:40 +0200

    Firmaluokka done

---
+++ Firmaluokka/Firmaluokka/Company.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Firmaluokka
+{
+    class Company
+    {
+        public string title;
+        public string address;
+        public string phone;
+        public double outcome;
+        public double expense;
+
+        public Company (string title, string address, string phone, double outcome, double expense)
+        {
+            this.title = title;
+            this.address = address;
+            this.phone = phone;
+            this.outcome = outcome;
+            this.expense = expense;
+        }
+
+        public void Print()
+        {
+            Console.WriteLine($"{this.title}");
+            Console.WriteLine($"{this.address}");
+            Console.WriteLine($"{this.phone}");
+            Console.WriteLine($"{this.outcome}");
+            Console.WriteLine($"{this.expense}");
+        }
+
+        public string Profitability()
+        {
+            double profit = (outcome - expense) / expense * 100;
+
+            if (profit < 100)
+                return "Firmalla menee kehnosti.";
+
+            else if (profit < 200)
+                return "Firmalla menee välttävästi.";
+
+            else if (profit < 300)
+                return "Firmalla menee tyydyttävästi.";
+
+            else
+                return "Firmalla menee hyvin.";
+        }
+
+        public Company (Company previousCompany)
+        {
+            title = previousCompany.title;
+            address = previousCompany.address;
+            phone = previousCompany.phone;
+            outcome = previousCompany.outcome;
+            expense = previousCompany.expense;
+        }
+    }
+}
---
+++ Firmaluokka/Firmaluokka/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Firmaluokka
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Firmaohjelma");
+            Console.WriteLine("------------------------------");
+
+            Company firstCompany = new Company("Nalle", "Metsä", "777", 33000, 18000);
+            firstCompany.Print();
+            Console.WriteLine(firstCompany.Profitability());
+    
+            Console.WriteLine("------------------------------");
+
+            Company secondCompany = new Company("Ahven", "Järvi", "666", 45000, 5500);
+            secondCompany.Print();
+            Console.WriteLine(secondCompany.Profitability());
+
+            Console.WriteLine("------------------------------");
+
+            Company thirdCompany = new Company("Koira", "Koppi", "999", 22000, 11000);
+            thirdCompany.Print();
+            Console.WriteLine(thirdCompany.Profitability());
+
+            Console.WriteLine("------------------------------");
+
+            Company fourthCompany = new Company(thirdCompany);
+            fourthCompany.Print();
+            Console.WriteLine(fourthCompany.Profitability());
+
+            Console.WriteLine("------------------------------");
+
+
+        }
+    }
+}
