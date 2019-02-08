5a203ce33972fdd25c332e4f72ee6718c8f7edbe
veerahentinen@hotmail.com <Styx1369>
2019-02-08 10:09:08 +0200

    Done

---
+++ Työntekijäluokka/Työntekijäluokka/Employee.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Työntekijäluokka
+{
+    class Employee
+    {
+        public string name;
+        public string id;
+        public string position;
+        public double salary;
+
+        public Employee (string name, string id, string position, double salary)
+        {
+            this.name = name;
+            this.id = id;
+            this.position = position;
+            this.salary = salary;
+        }
+
+        public void Print()
+        {
+            Console.WriteLine($"{this.name}");
+            Console.WriteLine($"{this.id}");
+            Console.WriteLine($"{this.position}");
+            Console.WriteLine($"{this.salary}");
+        }
+
+        public override string ToString()
+        {
+            return $"{name}, {id}, {position}, {salary}";
+        }
+
+        public static string CompareSalary(Employee firstEmployee, Employee secondEmployee)
+        {
+            if (firstEmployee.salary > secondEmployee.salary)
+                return $"{firstEmployee.name} tienaa enemmän kuin {secondEmployee.name}.";
+            else
+                return $"{secondEmployee.name} tienaa enemmän kuin {firstEmployee.name}.";
+
+        }
+
+    }
+}
---
+++ Työntekijäluokka/Työntekijäluokka/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Työntekijäluokka
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Työntekijäohjelma");
+            Console.WriteLine("------------------------------");
+
+            Employee[] employees = new Employee[3];
+            employees[0] = new Employee("Matti", "123", "Siivooja", 14.25);
+            employees[1] = new Employee("Seppo", "987", "Myyjä", 15.04);
+            employees[2] = new Employee("Liisa", "666", "Myymäläpäällikkö", 16.98);
+            
+            foreach (Employee person in employees)
+            {
+                Console.WriteLine(person.ToString());
+            }
+
+            Console.WriteLine("------------------------------");
+
+            Console.WriteLine(Employee.CompareSalary(employees[0], employees[1]));
+
+        }
+    }
+}
